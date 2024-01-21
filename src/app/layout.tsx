3ec5dd0201122ce65c82fbd834d651edc86e445a
Omkar Date <omkardate29@gmail.com>
2024-01-21 13:44:20 +0530

    feat: home page is ready

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import './globals.css';
 
 const font = Poppins({
-  subsets: ["devanagari"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ['devanagari'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 export const metadata: Metadata = {
-  title: "Scavenger Hunt",
+  title: 'Scavenger Hunt',
   description:
     "This event is organized by CESA Computer Engineering Students' Association, BVCOENM",
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import Image from 'next/image';
+
 export default function Page() {
-  return <main>Scavenger Hunt</main>;
+  return (
+    <main className='p-4'>
+      <h1 className='py-4 text-center text-2xl font-bold'>
+        Welcome to the Ultimate Scavenger Hunt!
+      </h1>
+      <Image
+        src='https://res.cloudinary.com/omkardate/image/upload/v1705478530/cesa/event/ongoing/scavenger/main.jpg'
+        width={500}
+        height={500}
+        alt='Scavenger Hunt'
+        className='h-full w-full object-cover object-center'
+        priority={true}
+      />
+    </main>
+  );
 }
