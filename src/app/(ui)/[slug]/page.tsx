b8967cb9fb577f8e0cb0e30aacf10b4b380782a2
Omkar Date <omkardate29@gmail.com>
2024-01-22 16:34:30 +0530

    fix: ui fixed

--- src/app/(ui)/[slug]/page.tsx
+++ src/app/(ui)/[slug]/page.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import Form from '@/components/form';
+import { useEffect } from 'react';
 
 type ParamsType = { params: { slug: string } };
 
 export default function Page({ params: { slug } }: ParamsType) {
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
+
   return (
     <div className='mx-6 my-8'>
       <Form id={slug} />
